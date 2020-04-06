396d0d196f5aad8ff7c2c149d1005fceb6524e89
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-06 15:19:48 -0400

    configured controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,48 @@
 class OrdersController < ApplicationController
+    before_action :set_order, only: [:show, :edit, :update, :destroy]
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @order.update(order_params)
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @order.destroy
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :description, :price)
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :item_orders
     end
   end
-        resources :items
+  resources :items
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
