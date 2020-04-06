1b64edae6da69d3dd3bcc3e328c224d5b306673f
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-06 14:14:39 -0400

    generated order_item model

---
+++ app/models/item_order.rb
@@ -0,0 +1,4 @@
+class ItemOrder < ApplicationRecord
+  belongs_to :order
+  has_many :items
+end
---
+++ db/migrate/20200406181217_create_item_orders.rb
@@ -0,0 +1,12 @@
+class CreateItemOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :item_orders do |t|
+      t.string :instructions
+      t.integer :quantity
+      t.references :order, foreign_key: true
+      t.boolean :gluten_free, default: false
+
+      t.timestamps
+    end
+  end
+end
